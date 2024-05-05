3d9efdc946f2c16a923b2f1d7c7bdf60af71994c
sudenursahin <sudynoor@gmail.com>
2024-05-05 18:10:35 +0300

    Thymeleaf spring MVC
    This project is a demonstration of basic usage of Thymeleaf in a web application. . In this project, we explore how to integrate Thymeleaf into a Spring Boot application to render dynamic web content.

---
+++ 01-thymeleafdemo/src/main/java/com/sudynoor/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,67 @@
+package com.sudynoor.springboot.thymeleafdemo.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+    // need a controller method to show initial Html form
+    @GetMapping("/showForm")
+    public String showForm(){
+        return "helloworld-form";
+    }
+
+    // need a controller method to process the Html form
+
+   @RequestMapping("/processForm")
+    public String processForm(){
+        return "helloworld";
+   }
+
+
+   // need a controller method to read form data and
+    // add data to the model
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model){
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "YO!" + theName;
+
+        // add message to the model
+        model.addAttribute("message",result);
+        return "helloworld";
+    }
+
+    @GetMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName ,
+                                          Model model){
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "hi bitch" + theName;
+
+        // add message to the model
+        model.addAttribute("message",result);
+        return "helloworld";
+    }
+
+
+
+
+
+
+}
